c5b2d2ff3b8cc9cc3de68d78b1b902002ab03ce6
Zeyad Zidan <iamzeyadzidan@gmail.com>
2023-03-04 10:24:12 +0200

    Documented pca.py

--- pca.py
+++ pca.py
@@ -1,19 +1,43 @@
 import numpy as np
 import pandas as pd
 
-# Choose the best dimensionality reduction, we reduce dimensionality on the training set only
-def reduce(dimensions, eigenvalues):
-    alpha = .8
+"""
+Auxiliary function to reduce dimensionality of the data matrix.
+
+@type dimensions: int
+@param dimensions: An integer indicating dimensionality of the data.
+
+@type eigenvalues: array[int]
+@param eigenvalues: A NumPy array of integers holding the eigenvalues of cov matrix.
+
+@type alpha: float
+@param alpha: A float type number to determine the accuracy.
+@default alpha: 0.8
+"""
+def reduce(dimensions, eigenvalues, alpha = 0.8):
+    # Choose the best dimensionality reduction, we reduce dimensionality on the training set only
     lambdasum = np.sum(eigenvalues)
     r = dimensions
     for i in range(dimensions, 0, -1):
         ratio = np.sum(eigenvalues[:i]) / lambdasum
         if ratio >= alpha:
             r = i
-    print(r)
+    print("=== === Data can be reduced to " + r + " dimensions. === ===")
     return r
 
 
+"""
+Applies the pca on a data matrix.
+
+@type data: np array
+@param data: A NumPy array
+
+@rtype: tuple
+@return: a tuple of (
+    A: @type Pandas.DataFrame, The projection matrix after applying PCA
+    mean: @type NumPy.array, The mean vector.
+    )
+"""
 def apply_pca(data):
     # Compute the mean vector
     mean = data.mean()
